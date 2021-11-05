40df1d41c0b5b01def6e959795e9addcf64d34c9
manolomillon <manolomillon@gmail.com>
2021-11-05 12:47:13 +0100

    Inicio PrimeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'primeng-sandbox';
+  title: string;
+
+  constructor() {
+  this.title = '';
+  }
+  upper(): void{ 
+    this.title = this.title.toUpperCase();
+  }
 }
